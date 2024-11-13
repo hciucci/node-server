ac2b646d24e0387b97812d308362460c804c4717
Haden Ciucci <hciucci@Hadens-Macbook-Air>
2024-11-13 17:12:22 -0500

    test

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-const musicData = require('./music-data.json');
-
-app.use('/images', express.static('images'));
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
